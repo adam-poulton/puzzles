66de70c49887a2d39778d63a8152dbf871be13a6
adamp <adam.poulton5@gmail.com>
2023-05-12 18:31:48 +1000

    Implements progress feedback

--- script.js
+++ script.js
@@ -3,33 +3,67 @@ const board = document.getElementById('game-board');
 const input = document.getElementById('word-input')
 const submitButton = document.getElementById('submit-button');
 const guessesDiv = document.getElementById('guesses');
-const guesses = [];
+const initialDate = '2023-01-01';
+let guesses = [];
 let words = [];
 let seeds = [];
+let answers = [];
 let answer = "";
+let letter = "";
 let puzzle = "";
 
 
-function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
+function shuffle(guesses, index) {
+    const saved = guesses[index]; // Store the element at the specified index
+    const remaining = guesses.filter((_, i) => i !== index); // Get the remaining elements
+    const shuffled = remaining.sort(() => Math.random() - 0.5); // Shuffle the remaining elements
+    const middleIndex = Math.floor(shuffled.length / 2); // Calculate the middle index
   
-    // While there remain elements to shuffle.
-    while (currentIndex != 0) {
+    // Insert the saved element in the middle of the shuffled array
+    const result = [
+      ...shuffled.slice(0, middleIndex),
+      saved,
+      ...shuffled.slice(middleIndex)
+    ];
   
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-  
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-    }
-  
-    return array;
+    return result;
+  }
+
+function getDaysElapsed(arbitraryDate) {
+    const currentDate = new Date();
+    const pastDate = new Date(arbitraryDate);
+    const timeDifference = currentDate.getTime() - pastDate.getTime();
+    const daysElapsed = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    return daysElapsed;
 }
 
+function getSavedGuesses() {
+    const savedGuesses = JSON.parse(localStorage.getItem('savedGuesses'));
+    return savedGuesses
+}
+
+function setSavedGuesses(guesses) {
+    localStorage.setItem('savedGuesses', JSON.stringify(guesses));
+}
 
 function generatePuzzle() {
-    puzzle = shuffle(seeds[Math.floor(Math.random() * seeds.length)].split("")).join("");
+    const lastElapsed = localStorage.getItem('lastElapsed');
+    const elapsed = getDaysElapsed(initialDate).toString();
+    if (lastElapsed === elapsed) {
+        let savedGuesses = getSavedGuesses();
+        console.log(savedGuesses);
+        if (savedGuesses !== null) {
+            guesses = savedGuesses;
+            updateGuessDisplay(guesses);
+        }
+    } else {
+        setSavedGuesses([]);
+        localStorage.setItem('lastElapsed', elapsed);
+    }
+
+    const index = elapsed % seeds.length;
+    answer = seeds[index];
+    puzzle = shuffle(answer.split(""), elapsed % answer.length).join("");
     for (let index = 0; index < board.children.length; index++) {
         const element = board.children[index];
         element.innerHTML = puzzle.charAt(index).toUpperCase()
@@ -42,6 +76,7 @@ function generatePuzzle() {
     }
 
 }
+
 
 function isValidGuess(string) {
     const guess = string.toLowerCase();
@@ -84,6 +119,9 @@ function isValidGuess(string) {
 
 }
 
+const updateGuessDisplay = function(guesses) {
+    guessesDiv.children[0].innerText = [...guesses].join(", ");
+}
 
 const checkGuess = function(event) {
     event.preventDefault();
@@ -91,7 +129,9 @@ const checkGuess = function(event) {
     clearError();
     if (isValidGuess(userInput)) {
         guesses.push(userInput.toLowerCase());
-        guessesDiv.children[0].innerText = [...guesses].join(", ");
+        setSavedGuesses(guesses);
+        updateGuessDisplay(guesses);
+        updateProgressBar();
     } else if (userInput.length > 0) {
         showError();
     }
@@ -104,24 +144,48 @@ const showError = () => {
     input.classList.add('error');
 }
 
+
 const clearError = () => {
     input.classList.remove('error');
 }
 
+const updateProgressBar = () => {
+    var container = document.querySelector('.progress-container');
+    var progress = document.querySelector('.progress');
+    var progressBar = document.querySelector('.progress-bar');
+    var progressGoal = document.querySelector('.progress-goal');
+    var progressValue = document.querySelector('.progress-value');
+    progress.style.width = Math.floor(100 * (guesses.length / answers.length)) + '%';
+    container.title = guesses.length + " / " + answers.length;
+    progressGoal.innerText = answers.length;
+    progressValue.innerText = guesses.length;
+    if (guesses.length === answers.length) {
+        progress.classList.add('complete')
+    }
+}
 
 
 async function loadWords() {
-    let response;
+    let response
     try {
         response = await fetch('words.json');
         words = await response.json();
         response = await fetch('seeds.json');
         seeds = await response.json();
         generatePuzzle();
+        await fetch('answers.json')
+            .then(response => response.json())
+            .then(data => {
+                answers = data[answer].filter(word => word.includes(answer.charAt(4)));
+                answers.push(answer);
+            })
+            .then(()=> updateProgressBar());
+
     } catch (error) {
-        console.error('Error:', error)
+        console.error('Error:', error);
     }
 }
+
 
 function setUp() {
     // load the word and seed list from their json files
@@ -175,5 +239,6 @@ function setUp() {
         }
     });
 }
+
 
 window.onload = setUp;
